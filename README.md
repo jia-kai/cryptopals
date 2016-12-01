e78fa93468a6feaec984d4c7305d7bf596e66cc2
jiakai <jia.kai66@gmail.com>
2016-12-01 23:11:59 +0800

    challenge 17

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # cryptopals
 
-My solutions to [the cryptopals crypto challenges](https://cryptopals.com/) in python.
+My solutions to [the cryptopals crypto challenges](https://cryptopals.com/) in
+python3.
+
+Run `./main.py` to run all solutions; run `./main.py -h` to see available
+options.
+
---
+++ cryptopals/set3.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+
+from .utils import challenge, assert_eq, open_resource, CipherError
+from .algo.block import (aes_ecb, pkcs7_pad, pkcs7_unpad, cbc_encrypt,
+                         cbc_decrypt, as_np_bytearr)
+from .bytearr import Bytearr
+
+import numpy as np
+
+@challenge
+def ch17():
+    class Server:
+        _data = None
+        _encr_impl = None
+        _decr_impl = None
+
+        def __init__(self):
+            self._data = data = []
+            with open_resource() as fin:
+                for line in fin:
+                    data.append(Bytearr.from_base64(line))
+            enc = aes_ecb(np.random.bytes(16))
+            self._encr_impl = enc.encryptor().update
+            self._decr_impl = enc.decryptor().update
+
+        def query(self):
+            iv = Bytearr(np.random.bytes(16), allow_borrow=True)
+            data = self._data[np.random.randint(len(self._data))]
+            data = pkcs7_pad(data)
+            return iv, cbc_encrypt(iv, data, self._encr_impl).np_data
+
+        def check(self, iv, ciphertext):
+            data = cbc_decrypt(iv, ciphertext, self._decr_impl)
+            try:
+                pkcs7_unpad(data)
+                return True
+            except CipherError:
+                return False
+
+    srv = Server()
+    iv, ciphertext = srv.query()
+    block_size = 16
+    assert len(ciphertext) % block_size == 0
+
+    def decr_block(ciphertext):
+        # decrypt a single block
+
+        def check_single(iv):
+            # assume (iv ^ plain)[-1] is '\x01'
+            plain = np.empty_like(iv)
+            plain[-1] = iv[-1] ^ 1
+            for pad_size in range(2, block_size + 1):
+                iv[-pad_size+1:] = plain[-pad_size+1:] ^ pad_size
+                found = False
+                for test in range(256):
+                    iv[-pad_size] = test
+                    if srv.check(iv, ciphertext):
+                        found = True
+                        break
+                if not found:
+                    return
+                plain[-pad_size] = iv[-pad_size] ^ pad_size
+            return plain
+
+        while True:
+            iv = as_np_bytearr(np.random.bytes(16))
+            if not srv.check(iv, ciphertext):
+                continue
+            ret = check_single(iv)
+            if ret is not None:
+                return ret
+
+    plain = []
+    for i in range(0, len(ciphertext), block_size):
+        cur = decr_block(ciphertext[i:i+block_size])
+        plain.extend(map(int, iv ^ cur))
+        iv = ciphertext[i:i+block_size]
+    return pkcs7_unpad(plain).decode('ascii')
