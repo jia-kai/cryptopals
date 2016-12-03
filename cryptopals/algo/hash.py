9a42cecdc15e1945c32691f35a1c441053ec61fa
jiakai <jia.kai66@gmail.com>
2016-12-03 17:18:50 +0800

    challenge 29

--- cryptopals/algo/hash.py
+++ cryptopals/algo/hash.py
@@ -1,65 +1,62 @@
 # -*- coding: utf-8 -*-
 
 from ..utils import as_bytes
+
+import numpy as np
+
 import struct
 
-def sha1(message):
-    """SHA-1 Hashing Function
+dt_bigu32 = np.dtype('>I')
+
+def md_padding(message, nbytes_off=0):
+    """padding used in Merkle–Damgård construction
+
+    :param nbytes_off: number of bytes to be assumed before given messsage
+    :return: bytes
+    """
+    message = as_bytes(message)
+    nbytes = len(message) + nbytes_off
+
+    message += b'\x80'
+    message += b'\x00' * ((56 - (nbytes + 1)) % 64)
+    message += struct.pack('>Q', nbytes * 8)
+
+    return message
+
+def sha1(message,
+         state=(0x67452301, 0xEFCDAB89, 0x98BADCFE, 0x10325476, 0xC3D2E1F0),
+         nbytes_off=0):
+    """
+    SHA-1 Hashing Function
     A custom SHA-1 hashing function implemented entirely in Python.
-    Arguments:
-        message: The input message string to hash.
-    Returns:
-        A hex SHA-1 digest of the input message.
+
+    :param message: The input message string to hash.
+    :param state: initial SHA-1 internal state
+    :return: SHA-1 digest bytes
     """
     # modified from https://github.com/ajalt/python-sha1
 
-    message = as_bytes(message)
+    warnings_filters = np.warnings.filters[:]
+    np.warnings.simplefilter("ignore", RuntimeWarning)
 
-    def _left_rotate(n, b):
-        return ((n << b) | (n >> (32 - b))) & 0xffffffff
+    def left_rotate(n, b):
+        return (n << np.uint32(b)) | (n >> np.uint32(32 - b))
 
-    # Initialize variables:
-    h0 = 0x67452301
-    h1 = 0xEFCDAB89
-    h2 = 0x98BADCFE
-    h3 = 0x10325476
-    h4 = 0xC3D2E1F0
+    state = np.array(state, dtype=np.uint32)
+    message = md_padding(message, nbytes_off)
+    assert len(message) % 64 == 0
 
-    # Pre-processing:
-    original_byte_len = len(message)
-    original_bit_len = original_byte_len * 8
-    # append the bit '1' to the message
-    message += b'\x80'
-
-    # append 0 <= k < 512 bits '0', so that the resulting message length (in
-    # bits) is congruent to 448 (mod 512)
-    message += b'\x00' * ((56 - (original_byte_len + 1) % 64) % 64)
-
-    # append length of message (before pre-processing), in bits, as 64-bit
-    # big-endian integer
-    message += struct.pack(b'>Q', original_bit_len)
-    # Process the message in successive 512-bit chunks:
-    # break message into 512-bit chunks
     for i in range(0, len(message), 64):
-        w = [0] * 80
-        # break chunk into sixteen 32-bit big-endian words w[i]
-        for j in range(16):
-            w[j] = struct.unpack(b'>I', message[i + j*4:i + j*4 + 4])[0]
-        # Extend the sixteen 32-bit words into eighty 32-bit words:
+        w = np.zeros(80, dtype=np.uint32)
+        w[:16] = np.fromstring(message[i:i+64], dtype=dt_bigu32)
         for j in range(16, 80):
-            w[j] = _left_rotate(w[j-3] ^ w[j-8] ^ w[j-14] ^ w[j-16], 1)
+            w[j] = left_rotate(w[j-3] ^ w[j-8] ^ w[j-14] ^ w[j-16], 1)
 
-        # Initialize hash value for this chunk:
-        a = h0
-        b = h1
-        c = h2
-        d = h3
-        e = h4
+        a, b, c, d, e = state
 
         for i in range(80):
             if 0 <= i <= 19:
-                # Use alternative 1 for f from FIPS PB 180-1 to avoid ~
-                f = d ^ (b & (c ^ d))
+                f = (b & c) | (~b & d)
                 k = 0x5A827999
             elif 20 <= i <= 39:
                 f = b ^ c ^ d
@@ -72,16 +69,11 @@ def sha1(message):
                 k = 0xCA62C1D6
 
             a, b, c, d, e = (
-                (_left_rotate(a, 5) + f + e + k + w[i]) & 0xffffffff,
-                a, _left_rotate(b, 30), c, d)
+                left_rotate(a, 5) + f + e + np.uint32(k) + w[i],
+                a, left_rotate(b, 30), c, d)
 
-        # sAdd this chunk's hash to result so far:
-        h0 = (h0 + a) & 0xffffffff
-        h1 = (h1 + b) & 0xffffffff
-        h2 = (h2 + c) & 0xffffffff
-        h3 = (h3 + d) & 0xffffffff
-        h4 = (h4 + e) & 0xffffffff
+        state += a, b, c, d, e
 
-    # Produce the final hash value (big-endian):
-    return struct.pack('>IIIII', h0, h1, h2, h3, h4)
+    np.warnings.filters[:] = warnings_filters
+    return state.astype(dt_bigu32)
 
--- cryptopals/set4.py
+++ cryptopals/set4.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from .utils import challenge, assert_eq
-from .algo.hash import sha1
+from .utils import challenge, assert_eq, as_bytes
+from .algo.hash import sha1, md_padding, dt_bigu32
 
 import numpy as np
 
@@ -28,4 +28,30 @@ def ch28():
 
     h = hashlib.sha1()
     h.update(msg)
-    assert_eq(h.digest(), sha1(msg))
+    assert_eq(h.digest(), sha1(msg).tostring())
+
+@challenge
+def ch29():
+    class Server:
+        def __init__(self):
+            self._key = np.random.bytes(np.random.randint(5, 60))
+
+        @property
+        def keylen(self):
+            return len(self._key)
+
+        def request(self, msg):
+            return sha1(self._key + as_bytes(msg)).tostring()
+
+
+    srv = Server()
+    msg = (b"comment1=cooking%20MCs;userdata=foo;"
+           b"comment2=%20like%20a%20pound%20of%20bacon")
+    mac0 = srv.request(msg)
+    payload = b";admin=true"
+    msg1 = md_padding(msg, srv.keylen) + payload
+    padded_size = ((len(msg) + srv.keylen-1)//64+1) * 64
+    mac1 = sha1(payload, state=np.fromstring(mac0, dtype=dt_bigu32),
+                nbytes_off=padded_size).tostring()
+
+    assert_eq(srv.request(msg1), mac1 + b'x')
