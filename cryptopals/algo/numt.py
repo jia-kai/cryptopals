2390ba7207427aacddaded6c74a7091090091784
jiakai <jia.kai66@gmail.com>
2016-12-05 23:04:11 +0800

    use gmpy2 to implement some functions in utils.numt and finish challenge 42

--- cryptopals/algo/numt.py
+++ cryptopals/algo/numt.py
@@ -2,20 +2,14 @@
 
 """number theory helper routines"""
 
-import math
 import numpy as np
+import gmpy2
+
+import math
 
 def powmod(a, p, m):
     """compute a ** b % m"""
-    assert p >= 0 and m >= 2, (p, m)
-    ans = 1
-    cur = a % m
-    while p:
-        if p & 1:
-            ans = (ans * cur) % m
-        p >>= 1
-        cur = (cur * cur) % m
-    return ans
+    return pow(a, p, m)
 
 def hex2int(hv):
     """convert hex string to large int"""
@@ -26,11 +20,11 @@ def int2hex(iv):
     """convert int value to hex"""
     return hex(iv)[2:]
 
-def bytes2int(bv, endian='little'):
+def bytes2int(bv, endian='big'):
     """convert bytes to large int"""
     return int.from_bytes(bv, endian)
 
-def int2bytes(iv, length=None, endian='little'):
+def int2bytes(iv, length=None, endian='big'):
     """convert bytes to large int"""
     assert isinstance(iv, int)
     if length is None:
@@ -52,63 +46,8 @@ def large_randint(n, rng=np.random):
     return bytes2int(np.random.bytes(nbytes)) % n
 
 
-class _PrimalityTestImpl:
-    _presel = None
-    """small primes for fast test"""
-
-    def __init__(self, presel_size):
-        self._presel = presel = [2]
-        cur = 2
-        while len(presel) < presel_size:
-            is_p = False
-            while not is_p:
-                cur += 1
-                is_p = True
-                i = iter(presel)
-                while True:
-                    iv = next(i)
-                    if cur % iv == 0:
-                        is_p = False
-                        break
-                    if iv * iv >= cur:
-                        break
-            presel.append(cur)
-
-    def __call__(self, n):
-        for i in self._presel:
-            if n % i == 0:
-                return False
-            if i * i >= n:
-                return True
-
-        # Miller–Rabin test
-        d = n - 1
-        s = 0
-        while not (d & 1):
-            s += 1
-            d >>= 1
-        mod_m1 = n - 1
-
-        # with error rate 4***(-60)
-        for _ in range(60):
-            a = int(np.random.randint(1 << 30))
-            ap = powmod(a, d, n)
-            if ap == 1:
-                continue
-
-            is_comp = True
-            for i in range(s):
-                if ap == mod_m1:
-                    is_comp = False
-                    break
-                ap = (ap * ap) % n
-
-            if is_comp:
-                return False
-
-        return True
-
-primep = _PrimalityTestImpl(100)
+def primep(n):
+    return gmpy2.is_prime(n)
 
 def gen_prime(bits, requirement=lambda x: True):
     """generate a prime of at least given bits long"""
@@ -122,24 +61,14 @@ def gen_prime(bits, requirement=lambda x: True):
         n += max(2, int(n.bit_length() * math.log(2))**2)
 
 def next_prime(n):
-    if not (n & 1):
-        n += 1
-    while not primep(n):
-        n += 2
-    return n
+    return int(gmpy2.next_prime(n))
 
 def egcd(a, b):
     """extended gcd
 
     :return: g, x, y such that x * a + y * b == g and x > 0
     """
-    if a == 0 or b == 0:
-        return a | b, 1, 1
-
-    k, r = divmod(a, b)
-    g, x1, y1 = egcd(b, r)
-
-    x, y = y1, x1 - k * y1
+    g, x, y = map(int, gmpy2.gcdext(a, b))
     x %= (b // g)
     y = (g - x * a) // b
     return g, x, y
@@ -176,15 +105,3 @@ def solve_congruences(rems, mods):
         cur_rem %= cur_mod
 
     return cur_mod, cur_rem
-
-def main():
-    from ..utils import assert_eq
-    print('512-bit prime:', gen_prime(512))
-    a = np.random.randint(2, 1<<16)
-    b = np.random.randint(2, 1<<16)
-    g, x, y = egcd(a, b)
-    assert_eq(a * x + b * y, g)
-    assert_eq(invmod(123, 4567) * 123 % 4567, 1)
-
-if __name__ == '__main__':
-    main()
--- cryptopals/set6.py
+++ cryptopals/set6.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 
-from .utils import challenge, assert_eq
-from .algo.numt import powmod, invmod, bytes2int
+from .utils import challenge, assert_eq, as_bytes
+from .algo.numt import powmod, invmod, bytes2int, int2bytes
 from .algo.asym import RSA
 
 import numpy as np
+import gmpy2
+
+import re
 
 @challenge
 def ch41():
@@ -18,3 +21,38 @@ def ch41():
     m0 = invmod(S, enc._n, m1)
 
     assert_eq(m0, msg)
+
+@challenge
+def ch42():
+    def make_pkcs15(msg, num=1):
+        return b'\x01' + (b'\xff' * num) + b'\x00ASN.1' + as_bytes(msg)
+
+    def pkcs15_check(padded, msg):
+        pat = b'\x01\xff+\x00ASN.1' + re.escape(as_bytes(msg))
+        return re.match(pat, as_bytes(padded)) is not None
+
+    assert_eq(pkcs15_check(make_pkcs15('x'), 'x'), True)
+    assert_eq(pkcs15_check(make_pkcs15('x', 2), 'x'), True)
+    assert_eq(pkcs15_check(make_pkcs15('x', 0), 'x'), False)
+    assert_eq(pkcs15_check(make_pkcs15('x'), 'y'), False)
+
+    enc, _ = RSA.make_enc_dec_pair(bits=1024, e=3)
+
+    msg = 'hi mom'
+    msg_pad = make_pkcs15(msg)
+    n = bytes2int(msg_pad)
+    n_upper = n + 1
+    while True:
+        n3, exact = gmpy2.iroot(n, 3)
+        n3 = int(n3)
+        if not exact:
+            n3 += 1
+        n33 = n3**3
+        assert n33 < enc._n
+        if n33 < n_upper:
+            break
+
+        n <<= 8
+        n_upper <<= 8
+
+    assert_eq(pkcs15_check(int2bytes(enc(n3)), msg), True)
